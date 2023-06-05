4d8ab2a50225836acd96500e5f3ee45a531cc356
jjm2473 <1129525450@qq.com>
2023-06-05 12:44:57 +0800

    rk35: tftp speed

--- include/configs/evb_rk3528.h
+++ include/configs/evb_rk3528.h
@@ -23,4 +23,15 @@
 #define CONFIG_BOOTCOMMAND RKIMG_BOOTCOMMAND
 #endif
 
+/* 
+ * TFTP speed up, by enable ip fragment, and set default blksize to MTU.
+ * Use `env set tftpblocksize 16384` to override blksize, max CONFIG_NET_MAXDEFRAG.
+ * MUST: CONFIG_TFTP_BLOCKSIZE <= CONFIG_NET_MAXDEFRAG
+ */
+#define CONFIG_IP_DEFRAG
+/* ip fragment, CONFIG_NET_MAXDEFRAG , default 16384, max 65536 */
+/* #define CONFIG_NET_MAXDEFRAG		16384 */
+#define CONFIG_UDP_CHECKSUM
+#define CONFIG_TFTP_BLOCKSIZE		1468
+
 #endif /* __CONFIGS_RK3528_EVB_H */
